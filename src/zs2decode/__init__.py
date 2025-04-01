d95137a2fdfff590ab4e7d74a8b544a221235adf
Chris Petrich <cpetrich@users.noreply.github.com>
2025-04-01 09:30:07 +0200

    update license expression

--- src/zs2decode/__init__.py
+++ src/zs2decode/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.3.3.dev0"
+__version__ = "0.3.3"
 
 __title__ = "zs2decode"
 __description__ = "read Zwick zs2 and zp2 files"
@@ -8,5 +8,5 @@ __uri__ = "https://zs2decode.readthedocs.org/"
 __author__ = "Chris Petrich"
 __email__ = "cpetrich@users.noreply.github.com"
 
-__license__ = "MIT"
-__copyright__ = "Copyright (C) 2015-2022 Chris Petrich"
+__license__ = "MIT License"
+__copyright__ = "Copyright (C) 2015-2025 Chris Petrich"
