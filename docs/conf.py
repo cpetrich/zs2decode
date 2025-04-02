6006ca565fb5dfba9694e354cce864477f2529ab
Chris Petrich <cpetrich@users.noreply.github.com>
2025-04-02 22:55:49 +0200

    remove unicode string designators

--- docs/conf.py
+++ docs/conf.py
@@ -47,9 +47,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'zs2decode'
-copyright = u'2015-2025, Chris Petrich'
-author = u'Chris Petrich'
+project = 'zs2decode'
+copyright = '2015-2025, Chris Petrich'
+author = 'Chris Petrich'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -228,8 +228,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'zs2decode.tex', u'zs2decode Documentation',
-   u'Chris Petrich', 'manual'),
+  (master_doc, 'zs2decode.tex', 'zs2decode Documentation',
+   'Chris Petrich', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -258,7 +258,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'zs2decode', u'zs2decode Documentation',
+    (master_doc, 'zs2decode', 'zs2decode Documentation',
      [author], 1)
 ]
 
@@ -272,7 +272,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'zs2decode', u'zs2decode Documentation',
+  (master_doc, 'zs2decode', 'zs2decode Documentation',
    author, 'zs2decode', 'One line description of project.',
    'Miscellaneous'),
 ]
